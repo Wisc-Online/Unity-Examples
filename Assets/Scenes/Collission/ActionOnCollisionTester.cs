6c719ba138b2e26f3ec9cb5cc034a1adcd491568
Brett Sheleski <brett@sheleski.com>
2018-09-07 14:16:38 -0500

    updated scene for updated Lib repo

---
+++ Assets/Scenes/Collission/ActionOnCollisionTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ActionOnCollisionTester : MonoBehaviour {
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    public void GoUp()
+    {
+        rb.velocity = Vector3.up * 10;
+    }
+}
---
+++ Assets/Scenes/Collission/PointerHandlerTester.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerHandlerTester : MonoBehaviour {
+    private Rigidbody rb;
+    private Renderer theRenderer;
+
+    private void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        this.theRenderer = GetComponent<Renderer>();
+    }
+
+    public void MoveUp()
+    {
+        rb.velocity = Vector3.up * 5;
+    }
+
+    public Material hoverMaterial, downMaterial, upMaterial, exitMaterial;
+
+    public void Enter()
+    {
+        theRenderer.material = hoverMaterial;
+    }
+
+    public void Exit()
+    {
+        theRenderer.material = exitMaterial;
+    }
+
+    public void Up()
+    {
+        theRenderer.material = upMaterial;
+    }
+
+    public void Down()
+    {
+        theRenderer.material = downMaterial;
+    }
+}
