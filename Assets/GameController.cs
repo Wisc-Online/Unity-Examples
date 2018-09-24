76f7482a122224db74483723d824f648dcc772f4
Brett Sheleski <brett@sheleski.com>
2018-09-24 12:06:32 -0500

    Updated example scene

---
+++ Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using FVTC.LearningInnovations.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : GameControllerBase
+{ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scenes/Collission/CollissionTesterSceneController.cs
@@ -0,0 +1,19 @@
+﻿using FVTC.LearningInnovations.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollissionTesterSceneController : SceneControllerBase
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
